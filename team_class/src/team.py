85af761e462f32d42b64149b3a10147f274d3a63
Esgrid <esikahall@gmail.com>
2021-11-22 14:41:39 +0000

    team and student class lab

---
+++ team_class/src/team.py
@@ -0,0 +1,22 @@
+class Team:
+    def __init__(self, name, players, coach):
+        self.name = name
+        self.players = players
+        self.coach = coach
+        self.points = 0
+        self.injured = 0
+
+    def add_player(self, new_player):
+        self.players.append(new_player)
+
+    def has_player(self, player):
+        return (True if player in self.players else False)
+
+    def play_game(self, won_or_not):
+        if won_or_not: 
+            self.points += 3
+        else:
+            self.points += 0
+    
+    def players_injured(self, players):
+        self.injured += len(players)
